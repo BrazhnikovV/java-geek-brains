a1ac08112d83fb1ddf45d51aabd3fb843bafc13c
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-09-06 13:09:41 +0300

    Разбиение массива на две части.

--- com/example/jcore/lesson_5/home_work/ArrayWorker.java
+++ com/example/jcore/lesson_5/home_work/ArrayWorker.java
@@ -14,7 +14,7 @@ public class ArrayWorker {
      *  @access private
      *  @var integer size
      */
-    private final int size = 10000000;
+    private final int size = 1000000/*0*/;
 
     /**
      *  @access private
@@ -23,21 +23,45 @@ public class ArrayWorker {
     private final int h = size / 2;
 
     /**
-     * firstGeneralMethod -
+     * firstGeneralMethod - первый основной метод домашнего задания
      *
-     * @return String
      */
     public void firstGeneralMethod () {
 
+        // засекаем начало время выполнения
+        notifyExecutionTime();
+
+        float[] arr = newValueForArrayElements (
+            fillArray( createArray() )
+        );
+
+        // засекаем окончание времени выполнения
+        notifyExecutionTime();
     }
 
     /**
-     * secondGeneralMethod -
+     * secondGeneralMethod - второй основной метод домашнего задания
      *
-     * @return String
      */
     public void secondGeneralMethod () {
 
+        // засекаем начало время выполнения
+        notifyExecutionTime();
+
+
+        float[] main_arr = fillArray( createArray() );
+        float[] first_part_arr = splitArrayTwoParts( main_arr, 1 );
+        float[] second_part_arr = splitArrayTwoParts( main_arr, 2 );
+
+
+        System.out.println( first_part_arr.length );
+        System.out.println( first_part_arr[first_part_arr.length - 1] );
+
+        System.out.println( second_part_arr.length );
+        System.out.println( second_part_arr[second_part_arr.length - 1] );
+
+        // засекаем окончание времени выполнения
+        notifyExecutionTime();
     }
 
     /**
@@ -70,10 +94,10 @@ public class ArrayWorker {
     /**
      * notifyExecutionTime - засечь время выполнения
      *
-     * @return long
      */
-    private long notifyExecutionTime () {
-        return System.currentTimeMillis();
+    private void notifyExecutionTime () {
+        // !!! Необходимо выводить разницу
+        System.out.println( System.currentTimeMillis() );
     }
 
     /**
@@ -92,5 +116,29 @@ public class ArrayWorker {
         }
 
         return arr;
+    }
+
+    /**
+     * splitArrayTwoParts - разделить массив на две части
+     *
+     * @param arr - длинный массив
+     * @param number_part - первая или вторая половина массива
+     * @return array
+     *
+     */
+    private float[] splitArrayTwoParts ( float[] arr, int number_part ) {
+
+        float[] half_arr = new float[h];
+
+        if ( number_part == 1 ) {
+            System.arraycopy( arr, 0, half_arr, 0, arr.length / 2 );
+            return half_arr;
+        }
+        if ( number_part == 2 ) {
+            System.arraycopy( arr, arr.length / 2, half_arr, 0, arr.length / 2 );
+            return half_arr;
+        }
+
+        return half_arr;
     }
 }
--- com/example/jcore/lesson_5/home_work/Main.java
+++ com/example/jcore/lesson_5/home_work/Main.java
@@ -1,6 +1,5 @@
 package com.example.jcore.lesson_5.home_work;
 
-import com.example.jcore.lesson_5.paint_elements_threads.BounceFrame;
 
 import javax.swing.*;
 import java.awt.*;
@@ -8,13 +7,9 @@ import java.awt.*;
 public class Main
 {
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame = new BounceFrame();
-                frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-                frame.setVisible( true );
-            }
-        });
+        ArrayWorker arr_worker = new ArrayWorker();
+
+        //arr_worker.firstGeneralMethod();
+        arr_worker.secondGeneralMethod();
     }
 }
