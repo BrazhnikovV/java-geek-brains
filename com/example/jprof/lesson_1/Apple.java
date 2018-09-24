7885e6be7d4c9758556e0f5ec58b45ce14a252c1
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-09-24 09:43:45 +0300

    Добавил два класса фруктов.

--- com/example/jprof/lesson_1/Apple.java
+++ com/example/jprof/lesson_1/Apple.java
@@ -1,4 +1,28 @@
 package com.example.jprof.lesson_1;
 
-public class Apple {
+/**
+ * Apple -
+ *
+ * @version 1.0.1
+ * @package com.example.jprof.lesson_1
+ * @author  Vasya Brazhnikov
+ * @copyright Copyright (c) 2018, Vasya Brazhnikov
+ */
+public class Apple extends Fruit {
+
+    /**
+     *  @access private
+     *  @var string name
+     */
+    private int weight;
+
+    /**
+     * constructor
+     *
+     * @param weight - вес фрукта
+     * @return undefined
+     */
+    public Apple ( int weight ) {
+        this.weight = weight;
+    }
 }
--- com/example/jprof/lesson_1/Orange.java
+++ com/example/jprof/lesson_1/Orange.java
@@ -1,4 +1,28 @@
 package com.example.jprof.lesson_1;
 
-public class Orange {
+/**
+ * Orange -
+ *
+ * @version 1.0.1
+ * @package com.example.jprof.lesson_1
+ * @author  Vasya Brazhnikov
+ * @copyright Copyright (c) 2018, Vasya Brazhnikov
+ */
+public class Orange extends Fruit {
+
+    /**
+     *  @access private
+     *  @var string name
+     */
+    private int weight;
+
+    /**
+     * constructor
+     *
+     * @param weight - вес фрукта
+     * @return undefined
+     */
+    public Orange ( int weight ) {
+        this.weight = weight;
+    }
 }
