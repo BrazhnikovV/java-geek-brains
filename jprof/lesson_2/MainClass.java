03b84c701707043cbf772f7ea392f87363d8bb87
BrazhnikovV <brazhnikov_ve@mail.ru>
2018-09-26 13:02:19 +0300

    Подготовил проект месенджера для домашнего задания для второй лекции.(java.core - проф).

---
+++ jprof/lesson_2/MainClass.java
@@ -0,0 +1,25 @@
+package jprof.lesson_2;
+
+import jcore.lesson_8.client.ClientController;
+
+/**
+ * MainClass - запускает контроллер клиента
+ *
+ * @version 1.0.1
+ * @package com.example.jcore.lesson_7.home_work
+ * @author  Vasya Brazhnikov
+ * @copyright Copyright (c) 2018, Vasya Brazhnikov
+ */
+public class MainClass {
+
+    /**
+     * main -
+     * @param args -
+     */
+    public static void main( String[] args ) {
+
+        // Запускаем контроллер клиента,
+        // он же инициализирует окно пользовательского интерфейса
+        ClientController controller = new ClientController();
+    }
+}
